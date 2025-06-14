25eef8df4337b798571f2c64823f32a84144a94c
yorsh1111 <156119981+garir1111@users.noreply.github.com>
2025-06-15 06:49:56 +0900

    initial

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import { Hono } from "hono";
+import { Resend } from "resend";
+import { EmailTemplate } from "./emails/email-template";
+
+const app = new Hono();
+// ここは発行されたAPIキーを書く
+const resend = new Resend("re_xxx");
+
+app.get("/", async (c) => {
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["garireodragon@gmail.com"],
+    subject: "hello world",
+    react: <EmailTemplate firstName="Maki" />,
+  });
+
+  if (error) {
+    return c.json(error, 400);
+  }
+
+  return c.json(data);
+});
+
+export default app;
